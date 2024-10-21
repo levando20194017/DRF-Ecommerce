2559e385e18298c385ed8199dda879ed1a2dbd69
DO Le Van VTI.D9 <do.levan@vti.com.vn>
2024-10-21 18:48:26 +0700

    feat: api guest get detail order

--- drfecommerce/drfecommerce/apps/order_detail/views.py
+++ drfecommerce/drfecommerce/apps/order_detail/views.py
@@ -1,16 +1,55 @@
 from rest_framework import viewsets
 from .models import OrderDetail
 from .serializers import OrderDetailSerializer
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from drfecommerce.apps.guest.models import Guest
+from drfecommerce.apps.order.models import Order
+from rest_framework.permissions import IsAuthenticated
+from drfecommerce.apps.guest.authentication import GuestSafeJWTAuthentication
+from rest_framework.decorators import action
 
 class OrderDetailViewSet(viewsets.ModelViewSet):
     queryset = OrderDetail.objects.all()
     serializer_class = OrderDetailSerializer
+    authentication_classes = [GuestSafeJWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    #api view order detail
+    @action(detail=False, methods=['get'], url_path="get-order-detail")
+    def get_order_detail(self, request):
+        """
+        Parameters:
+        - order_id: ID or the order
+        """
+        order_id = request.GET.get('order_id')
 
-    def create(self, request):
-        # Step 1: Validate and create order detail
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        order_detail = serializer.save()
+        if not order_id:
+            return Response({
+                "status": status.HTTP_400_BAD_REQUEST,
+                "message": "Order ID is required."
+            }, status=status.HTTP_400_BAD_REQUEST)
 
-        # Step 2: Respond with created order detail
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        try:
+            order = Order.objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return Response({
+                "status": status.HTTP_404_NOT_FOUND,
+                "message": "Order not found."
+            }, status=status.HTTP_404_NOT_FOUND)
+            
+        try:
+            order_detail = OrderDetail.objects.get(order=order)
+            serializer = OrderDetailSerializer(order_detail)
+            return Response({
+                "status": status.HTTP_200_OK,
+                "data": serializer.data
+            }, status=status.HTTP_200_OK)
+        except OrderDetail.DoesNotExist:
+            return Response({
+                "status": status.HTTP_404_NOT_FOUND,
+                "message": "Order detail not found."
+            }, status=status.HTTP_404_NOT_FOUND)
+            
+            
+
+    
