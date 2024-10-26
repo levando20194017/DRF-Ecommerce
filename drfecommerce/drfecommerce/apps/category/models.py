15b8a498e296157f4734267216db64e414c76ea4
levando20194017 <levando20194017@gmail.com>
2024-10-26 15:06:04 +0700

    feat: api tag and category

--- drfecommerce/drfecommerce/apps/category/models.py
+++ drfecommerce/drfecommerce/apps/category/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    image = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True,)
+    image = models.CharField(max_length=255, null=True, blank=True,)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
     delete_at = models.DateTimeField(null=True, blank=True, default=None)
