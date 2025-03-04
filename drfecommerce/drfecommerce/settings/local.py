1c2aea3b12d0cf5beca13db0ae303c0fcfeb6fbe
levando20194017 <levando20194017@gmail.com>
2025-03-04 21:47:38 +0700

    deploy DB to supabase

--- drfecommerce/drfecommerce/settings/local.py
+++ drfecommerce/drfecommerce/settings/local.py
@@ -1,5 +1,4 @@
 from .base import *
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -11,13 +10,28 @@ from .base import *
 #     }
 # }
 
+
+#local
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'EcommerceDjango',  # Tên database
+#         'USER': 'postgres',  # Tên người dùng của DB
+#         'PASSWORD': '123456',  # Mật khẩu của DB
+#         'HOST': 'localhost',  # Hostname, thường là localhost
+#         'PORT': '5432',  # Cổng mặc định của PostgreSQL là 5432
+#     }
+# }
+
+#supabase
+#postgresql://postgres:[YOUR-PASSWORD]@db.xdgoxqscdzswkdvengoi.supabase.co:5432/postgres
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'EcommerceDjango',  # Tên database
-        'USER': 'postgres',  # Tên người dùng của DB
-        'PASSWORD': '123456',  # Mật khẩu của DB
-        'HOST': 'localhost',  # Hostname, thường là localhost
+        'NAME': 'postgres',  # Tên database
+        'USER': 'postgres.xdgoxqscdzswkdvengoi',  # Tên người dùng của DB
+        'PASSWORD': os.environ.get("DB_PASSWORD"),  # Mật khẩu của DB
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',  # Hostname, thường là localhost
         'PORT': '5432',  # Cổng mặc định của PostgreSQL là 5432
     }
 }
